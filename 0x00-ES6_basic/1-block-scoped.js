d10123756a872f50277632a73dc7bc776f805460
Charles Lughas <lordlughas@gmail.com>
2023-06-27 18:53:13 +0000

    Add task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
-export default function taskBlock(trueOrFalse){
-  let task = false;
-  let task2 = true;
-    
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
   let task = true;
   let task2 = false;
