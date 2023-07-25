9d94435bb9038f5b2846212dc5f5dff8e2abe3dd
Charles Lughas <lordlughas@gmail.com>
2023-07-25 14:12:46 +0000

    Updated value of key -income in budget

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,7 +1,7 @@
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
-    income: '$199,868',
+    income: '$119,868',
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
