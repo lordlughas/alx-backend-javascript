9f950b766222df3ec92b9cdfd283a3f0417f1c8b
Charles Lughas <lordlughas@gmail.com>
2023-08-03 10:26:32 +0100

    Add Classes task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(cd) {
+    this._code = cd;
+  }
+  
+  get name() {
+    return this._name;
+  }
+
+  set name(nm) {
+    this._name = nm;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
