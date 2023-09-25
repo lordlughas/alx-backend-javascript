b3a40e92336937475e2ebca78606d53aca901e43
Charles Lughas <lordlughas@gmail.com>
2023-09-25 19:07:05 +0100

    Create program to use Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+// import readline module
+const readline = require('readline');
+
+// create interface for input and output
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// question user to enter name
+r1.question('Welcome to Holberton School, what is your name\n', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  console.log('This important software is now closing');
+  r1.close();
+});
