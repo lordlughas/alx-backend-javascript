e82aa7aa176a97de6a6c868fe894ab0f19da3dc3
Charles Lughas <lordlughas@gmail.com>
2023-09-25 19:46:49 +0100

    Fixed program output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@ const r1 = readline.createInterface({
 });
 
 // question user to enter name
-r1.question('Welcome to Holberton School, what is your name\n', (answer) => {
+r1.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
   console.log('This important software is now closing');
   r1.close();
