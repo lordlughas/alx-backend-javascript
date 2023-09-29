456ee05291da3affa161632a607e20471bf35b09
Charles Lughas <lordlughas@gmail.com>
2023-09-29 01:25:07 +0100

    Createa small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get(('/'), (req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server started running on port ${port}`);
+})
+
+module.exports = app;
